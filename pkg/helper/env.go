fc35b1787d8cfd9114e0b0c1ac8a118d9042cc0f
joey <joey@mail>
2023-08-30 16:53:22 +0800

    add helper GetOsEnvBool

--- pkg/helper/env.go
+++ pkg/helper/env.go
@@ -19,7 +19,7 @@ func GetOsEnvInt(key string, defaultValue int) int {
 		if num, err := strconv.Atoi(value); err == nil {
 			return num
 		} else {
-			panic(eris.Wrap(err, ""))
+			panic(eris.Wrapf(err, "key:%s value:%s", key, value))
 		}
 	}
 	return defaultValue
@@ -30,7 +30,18 @@ func GetOsEnvInt64(key string, defaultValue int64) int64 {
 		if num, err := strconv.ParseInt(value, 10, 64); err == nil {
 			return num
 		} else {
-			panic(eris.Wrap(err, ""))
+			panic(eris.Wrapf(err, "key:%s value:%s", key, value))
+		}
+	}
+	return defaultValue
+}
+
+func GetOsEnvBool(key string, defaultValue bool) bool {
+	if value, ok := os.LookupEnv(key); ok {
+		if b, err := strconv.ParseBool(value); err == nil {
+			return b
+		} else {
+			panic(eris.Wrapf(err, "key:%s value:%s", key, value))
 		}
 	}
 	return defaultValue
