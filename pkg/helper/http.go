6612e1fa2381ae996b9b5b8f6185609e750f525d
joey <joey@mail>
2024-06-17 13:52:12 +0800

    add helper

--- pkg/helper/http.go
+++ pkg/helper/http.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/MinamiKotoriCute/serr"
 )
@@ -18,4 +19,30 @@ func GetHttpQueryInt64(r *http.Request, key string, defaultValue int64) (int64, 
 	} else {
 		return i, nil
 	}
+}
+
+func GetHttpQuerySliceInt64(r *http.Request, key string, defaultValue []int64, splitSeparators string) ([]int64, error) {
+	value := r.URL.Query().Get(key)
+	if value == "" {
+		return defaultValue, nil
+	}
+
+	result := []int64{}
+	for _, s := range strings.Split(value, splitSeparators) {
+		if i, err := strconv.ParseInt(s, 10, 64); err != nil {
+			return nil, serr.Wrap(err)
+		} else {
+			result = append(result, i)
+		}
+	}
+
+	return result, nil
+}
+
+func GetHttpQueryString(r *http.Request, key string, defaultValue string) string {
+	value := r.URL.Query().Get(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
 }
